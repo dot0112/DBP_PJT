556d3fee65e18926c069f314e5e9bd5cbf8f0aac
DESKTOP-6B7URQN\deu <dot0112@naver.com>
2024-11-29 21:07:48 +0900

    Merge oh-young-min's code

---
+++ src/main/java/DBP_equipmentRentalService/main/controller/ReturnController.java
@@ -0,0 +1,14 @@
+package DBP_equipmentRentalService.main.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+
+@Controller
+public class ReturnController {
+    @GetMapping ("/return")
+    public String returnitem(Model model){
+        model.addAttribute("contentFragment", "return");
+        return "layout";
+    }
+}

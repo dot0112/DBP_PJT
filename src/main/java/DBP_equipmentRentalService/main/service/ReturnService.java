d03f8f821928f52c01148309311e4a6e2e847abe
Ohyoungmin <oym0217@naver.com>
2024-12-05 22:35:56 +0900

    need RentalRepository.findByUserId need ReturnReposiroty.findByRentalId

--- src/main/java/DBP_equipmentRentalService/main/service/ReturnService.java
+++ src/main/java/DBP_equipmentRentalService/main/service/ReturnService.java
@@ -1,6 +1,7 @@
 package DBP_equipmentRentalService.main.service;
 
 import DBP_equipmentRentalService.main.repository.item.ItemRepository;
+import DBP_equipmentRentalService.main.repository.rental.RentalRepository;
 import DBP_equipmentRentalService.main.repository.returns.ReturnsRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -9,10 +10,12 @@ import org.springframework.stereotype.Service;
 public class ReturnService {
     private final ReturnsRepository returnsRepository;
     private final ItemRepository itemRepository;
+    private final RentalRepository rentalRepository;
 
     @Autowired
-    public ReturnService(ReturnsRepository returnsRepository, ItemRepository itemRepository){
+    public ReturnService(ReturnsRepository returnsRepository, ItemRepository itemRepository, RentalRepository rentalRepository){
         this.returnsRepository = returnsRepository;
         this.itemRepository = itemRepository;
+        this.rentalRepository = rentalRepository;
     }
 }
